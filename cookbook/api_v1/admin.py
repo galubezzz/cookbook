437d5712808bb848bbeeb78488ccb74429dda7e7
Maksim Buturlakin <skvozik@gmail.com>
2020-10-14 22:13:21 -0700

    Add inline models in Recipe

--- cookbook/api_v1/admin.py
+++ cookbook/api_v1/admin.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 from webapp.models import Recipe, Ingredient, Step, Tag
 # Register your models here.
 
-admin.site.register(Recipe)
+
+class StepInline(admin.StackedInline):
+    model = Step
+
+
+class IngredientInline(admin.StackedInline):
+    model = Ingredient
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [StepInline, IngredientInline,]
+
+
+admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient)
 admin.site.register(Step)
 admin.site.register(Tag)
