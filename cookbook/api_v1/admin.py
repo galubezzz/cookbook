2352b3f86fcb6669e3bb10be4006bdf8ae883f14
galubezzz <galubezzz@gmail.com>
2020-10-14 20:01:54 -0700

    register Recipe, Tag, Igredient and Step models in admin

--- cookbook/api_v1/admin.py
+++ cookbook/api_v1/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from webapp.models import Recipe, Ingredient, Step, Tag
 # Register your models here.
+
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(Step)
+admin.site.register(Tag)
