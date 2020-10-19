0b090c02f3c5e1f667728b865a58060f498a14ce
galubezzz <galubezzz@gmail.com>
2020-10-19 16:10:51 -0700

    add api urls for Recipe, Step, Ingredient and Tag

--- cookbook/api_v1/urls.py
+++ cookbook/api_v1/urls.py
@@ -3,13 +3,18 @@ from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 from api_v1 import views
 
-router = routers.DefaultRouter()
 
+router = routers.DefaultRouter()
+router.register(r'tags', views.TagViewSet)
+router.register(r'recipes', views.RecipeViewSet)
+router.register(r'steps', views.StepViewSet)
+router.register(r'ingredients', views.IngredientViewSet)
 app_name = 'api_v1'
 
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('login/', obtain_auth_token, name='api_token_auth'),
-    path('register', views.UserCreateView.as_view(), name='register'),
+    path('register/', views.UserCreateView.as_view(), name='register'),
+
 ]
