07b11d25e46c48ae463c9dde334e2c0da4961a2a
galubezzz <galubezzz@gmail.com>
2020-10-19 16:09:08 -0700

    add viewsets for Step, Recipe, Ingredient and Tag

--- cookbook/api_v1/views.py
+++ cookbook/api_v1/views.py
@@ -2,10 +2,32 @@ from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from django.contrib.auth.models import User
-from api_v1.serializers import UserSerializer
+from api_v1.serializers import UserSerializer, TagSerializer, RecipeSerializer, IngredientSerializer, StepSerializer
+from webapp.models import Tag, Recipe, Ingredient, Step
+from rest_framework import viewsets
 
 
 class UserCreateView(CreateAPIView):
     model = User
     serializer_class = UserSerializer
     permission_classes = [AllowAny,]
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all().order_by('-id')
+    serializer_class = TagSerializer
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all().order_by('-id')
+    serializer_class = RecipeSerializer
+
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all().order_by('-id')
+    serializer_class = IngredientSerializer
+
+
+class StepViewSet(viewsets.ModelViewSet):
+    queryset = Step.objects.all().order_by('-id')
+    serializer_class = StepSerializer
