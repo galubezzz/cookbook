89cc521da841611d5512a6c247bb82af0706eb2c
galubezzz <galubezzz@gmail.com>
2020-11-02 18:27:04 -0800

    add tags field for recipies

--- frontend/src/components/AddRecipe.js
+++ frontend/src/components/AddRecipe.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import { withRouter } from 'react-router-dom';
+import ReactTags from 'react-tag-autocomplete'
 // import DjangoCSRFToken from 'django-react-csrftoken'
 
 function AddRecipe(props) {
@@ -8,10 +9,24 @@ function AddRecipe(props) {
     const [saved, setSaved] = useState(false);
     const [message, setMessage] = useState('');
     const [fileUploaded, setFileUploaded] = useState(false);
+    const [suggestions, setSuggestions] = useState([]);
+    const [tags, setTags] = useState([])
 
     useEffect(() => {
         console.log('--recipe', recipe);
-    });
+        setTags(
+            [
+        { id: 1, name: "Apples" },
+        { id: 2, name: "Pears" }]
+        );
+        setSuggestions(
+            [
+        { id: 3, name: "Bananas" },
+        { id: 4, name: "Mangos" },
+        { id: 5, name: "Lemons" },
+        { id: 6, name: "Apricots" }
+      ])
+    }, []);
 
     function changeName(event) {
         console.log('--- change name event', event.target.value);
@@ -20,6 +35,18 @@ function AddRecipe(props) {
             name: event.target.value
         });
     }
+
+    function onDelete (i) {
+    const _tags = tags.slice(0);
+    _tags.splice(i, 1);
+    setTags(_tags)
+  }
+
+  function onAddition (tag) {
+        console.log(tag)
+    const _tags = [].concat(tags, tag);
+    setTags(_tags)
+  }
 
     function changeDesc(event) {
         console.log('--- change desc event', event.target.value);
@@ -41,6 +68,7 @@ function AddRecipe(props) {
 
     function saveRecipe(event) {
         event.preventDefault();
+            console.log("bla")
         const url = 'http://127.0.0.1:8000/api/v1/recipes/';
         const data = new FormData();
         if (fileUploaded) {
@@ -71,7 +99,7 @@ function AddRecipe(props) {
                     <p>
                         {message}
                     </p>
-                    <form>
+
                         <div class="form-group row">
                             <label htmlFor="name" class="col-sm-1 col-form-label">Name:</label>
                             <div class="col-sm-8">
@@ -92,12 +120,20 @@ function AddRecipe(props) {
                                 <input name="pic" class="form-control-file" id="pic" type="file" onChange={changeFile}/>
                             </div>
                         </div>
+                        <ReactTags
+                            tags={tags}
+                            suggestions={suggestions}
+                            onDelete={onDelete}
+                            onAddition={onAddition}
+                            allowNew={true}
+                        />
                         <div className="form-group row">
                             <div className="col-sm-9">
                                 <button class="btn btn-primary btn-block" onClick={saveRecipe}> Save Recipe</button>
                             </div>
                         </div>
-                    </form>
+
+
                 </div>
             </div>
         </>
