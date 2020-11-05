255339f854e56970f56a1cf5a415e72217b116bf
galubezzz <galubezzz@gmail.com>
2020-11-04 17:25:56 -0800

    add component for checking if the user is authorized

---
+++ frontend/src/components/CheckAuth.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function CheckAuth(props) {
+    if (false){
+        return(
+            props.children
+        )
+    } else {
+        return "stas vrednyi";
+    }
+}
