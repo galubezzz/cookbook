553566e8e01fc291cb8c34d59f3ce00838767f65
galubezzz <galubezzz@gmail.com>
2020-11-04 18:19:40 -0800

    add ingredients and steps on the recipe details page

--- frontend/src/components/RecipeDetails.js
+++ frontend/src/components/RecipeDetails.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {Link} from "react-router-dom";
+import Ingredient from "./Ingredient";
+import Step from "./Step";
 
 const getRecipeURL = (id) => `http://127.0.0.1:8000/api/v1/recipes/${id}/`;
 
@@ -22,6 +24,10 @@ export default function RecipeDetails(props) {
             <div>{recipe.description}</div>
             <img src={recipe.pic}/>
             <p/>
+            {recipe.ingredient_in_recipe ?
+                recipe.ingredient_in_recipe.map((ingredient)=>{return <Ingredient ingredient={ingredient}/>}) : null}
+            {recipe.step_in_recipe ?
+            recipe.step_in_recipe.map((step)=>{return <Step step={step}/>}) : null}
             <Link to={`/edit-recipe/${recipe.id}`}>Edit</Link>
         </div>
     ) : null;
