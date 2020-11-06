ba31eb8c87c1fe9b0526fe813e2b1e4cb507e1e1
galubezzz <galubezzz@gmail.com>
2020-11-05 21:17:49 -0800

    add authorization token to headers

--- frontend/src/components/RecipeList.js
+++ frontend/src/components/RecipeList.js
@@ -2,10 +2,12 @@ import React, {useState, useEffect} from "react";
 import axios from 'axios'
 import Recipe from "./Recipe";
 
-export default function RecipeList() {
+export default function RecipeList(props) {
     const [recipes, setRecipes] = useState([]);
+    const url = "http://127.0.0.1:8000/api/v1/recipes/";
+    const token = props.user.token;
     useEffect(()=>{
-        axios.get("http://127.0.0.1:8000/api/v1/recipes/").then((response)=>{
+        axios.get(url, { headers: {"Authorization" : `Token ${token}`} }).then((response)=>{
         console.log("it's mine", response.data);
         setRecipes(response.data);
         })
