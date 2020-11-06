e4b86bb2de3be2a29d2228629e2a161fc3f4a195
galubezzz <galubezzz@gmail.com>
2020-11-05 19:57:44 -0800

    save username and token to state after login

--- frontend/src/components/UserLogin.js
+++ frontend/src/components/UserLogin.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
-import { withRouter } from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 
 function UserLogin(props) {
     const [user, setUser] = useState({});
@@ -8,35 +8,43 @@ function UserLogin(props) {
     const [saved, setSaved] = useState(false);
     const [message, setMessage] = useState('');
     const url = "http://127.0.0.1:8000/api/v1/login/";
-    function changeUsername(event){
+
+    function changeUsername(event) {
         setUser({
             ...user,
             username: event.target.value,
         });
         console.log("--username", user.username);
     }
-    function changePassword(event){
+
+    function changePassword(event) {
         setUser({
             ...user,
             password: event.target.value,
         });
     }
+
     function loginUser(event) {
         event.preventDefault();
-        axios.post(url, user).then((response)=>{
+        axios.post(url, user).then((response) => {
             console.log("--response", response);
-            if (response.status === 200){ // 200 == '200' - true
+            if (response.status === 200) { // 200 == '200' - true
                 // 200 === '200' - false
                 localStorage.setItem('auth-token', response.data.token);
                 localStorage.setItem('username', response.data.username);
                 console.log(localStorage['auth-token']);
+                props.onLogin({
+                    token: response.data.token,
+                    username: response.data.username,
+                });
                 props.history.push("/");
             }
-        }).catch((error)=>{
+        }).catch((error) => {
             console.log(error.response.data);
-            })
+        })
     }
-    return(
+
+    return (
         <form>
             <div className="form-group row">
                 <label htmlFor="name" className="col-sm-1 col-form-label">Username:</label>
@@ -47,7 +55,8 @@ function UserLogin(props) {
             <div className="form-group row">
                 <label htmlFor="name" className="col-sm-1 col-form-label">Password:</label>
                 <div className="col-sm-8">
-                    <input name="name" id="name" type="password" className="form-control pr-2" onChange={changePassword}/>
+                    <input name="name" id="name" type="password" className="form-control pr-2"
+                           onChange={changePassword}/>
                 </div>
             </div>
             <div className="form-group row">
@@ -58,4 +67,5 @@ function UserLogin(props) {
         </form>
     )
 }
+
 export default withRouter(UserLogin)
