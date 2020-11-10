d5747e2f66548a155da1a18bdbfc3bbc51b72f41
galubezzz <galubezzz@gmail.com>
2020-11-09 23:50:12 -0800

    add component for user account

---
+++ frontend/src/components/UserAccount.js
@@ -0,0 +1,28 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import {withRouter, Link} from "react-router-dom";
+
+const getUserURL = `http://127.0.0.1:8000/api/v1/users/`;
+
+function UserAccount(props) {
+    const [user, setUser] = useState();
+    const token = props.user.token;
+    useEffect(() => {
+        axios.get(getUserURL, {headers: {"Authorization": `Token ${token}`}}).then((response) => {
+            console.log("---response", response.data)
+            setUser(response.data[0]);
+            console.log(user);
+        })
+    }, []);
+    return user ? (
+
+        <>
+            <h1>My account details</h1>
+            <div>{user.username}</div>
+            <div>{user.email}</div>
+            <Link to="/edit-user-details">Edit</Link>
+        </>
+    ) : null
+}
+
+export default withRouter(UserAccount)
