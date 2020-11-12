246e4ee8885d19d0be70bbf555d259c4ea745c4f
galubezzz <galubezzz@gmail.com>
2020-11-11 19:34:22 -0800

    add component for editing step

---
+++ frontend/src/components/EditStep.js
@@ -0,0 +1,119 @@
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
+import {withRouter} from "react-router-dom";
+
+const url = (id) => `http://127.0.0.1:8000/api/v1/steps/${id}/`;
+
+function EditStep(props) {
+    const [step, setStep] = useState([]);
+    const [message, setMessage] = useState("");
+    const id = props.match.params.id;
+    const [fileUploaded, setFileUploaded] = useState(false);
+    const [saved, setSaved] = useState(false);
+    const token = props.user.token
+    useEffect(() => {
+        axios.get(url(id)).then((response) => {
+            if (response.status === 200) {
+                console.log('--response', response.data);
+                setStep(response.data)
+            } else {
+                setMessage("Could not load data")
+            }
+        })
+    }, []);
+
+    function changeName(event) {
+        setStep({
+            ...step,
+            name: event.target.value,
+        })
+    }
+
+    function changeDesc(event) {
+        setStep({
+            ...step,
+            description: event.target.value,
+        })
+    }
+
+    function changeNumber(event) {
+        setStep({
+            ...step,
+            step_number: event.target.value,
+        })
+    }
+
+    function changeFile(event) {
+
+        setStep({
+            ...step,
+            pic: event.target.files[0]
+        });
+        setFileUploaded(true);
+    }
+
+    function saveStep(event) {
+        event.preventDefault();
+        const data = new FormData();
+        if (fileUploaded) {
+            data.append('pic', step.pic, step.pic.name);
+        }
+        data.append('name', step.name);
+        data.append('description', step.description);
+        data.append('step_number', step.step_number);
+        data.append('recipe', id);
+
+        axios.patch(url(id), data, {headers: {"Authorization": `Token ${token}`}})
+            .then((response) => {
+                console.log('--response', response);
+                if (response.status === 200) {
+                    setSaved(true);
+                    props.history.push(`/recipe/${response.data.recipe}`)
+
+                } else {
+                    setMessage(`was not saved: ${JSON.stringify(response)}`);
+                }
+            });
+    }
+
+    return (
+        <>
+            <form>
+                <div className="form-group row">
+                    <label htmlFor="name" className="col-sm-1 col-form-label">Name:</label>
+                    <div className="col-sm-8">
+                        <input name="name" id="name" className="form-control pr-2" onChange={changeName}
+                               value={step.name}/>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label htmlFor="description" className="col-sm-1 col-form-label">Description:</label>
+                    <div className="col-sm-8">
+                <textarea name="description" className="form-control" id="description" aria-multiline="true"
+                          onChange={changeDesc} value={step.description}/>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label htmlFor="number" className="col-sm-1 col-form-label">Step number:</label>
+                    <div className="col-sm-8">
+                        <input name="number" id="number" type="number" step="0.01" className="form-control pr-2"
+                               onChange={changeNumber} value={step.step_number}/>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label htmlFor="pic" className="col-sm-1 col-form-label">Pic:</label>
+                    <div className="col-sm-8">
+                        <input name="pic" className="form-control-file" id="pic" type="file" onChange={changeFile}/>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <div className="col-sm-9">
+                        <button className="btn btn-primary btn-block" onClick={saveStep}> Save Step</button>
+                    </div>
+                </div>
+            </form>
+        </>
+    )
+}
+
+export default withRouter(EditStep)
