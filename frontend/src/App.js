46b264c80e8f19e220a55d485a2c96ce8fe7af45
galubezzz <galubezzz@gmail.com>
2020-11-16 17:30:08 -0800

    fix navbar layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,7 +41,7 @@ const reducer = (state, action) => {
     }
 };
 
-function logout(dispatch){
+function logout(dispatch) {
     dispatch({type: 'logout'});
     localStorage.clear();
 }
@@ -53,65 +53,77 @@ function App() {
 
     return (
         <UserContext.Provider value={state.user}>
-        <Router>
-            <nav className="navbar navbar-expand-sm navbar-light bg-light">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <NavLink to="/" activeClassName="active" className="nav-link">Home</NavLink>
-                    </li>
+            <Router>
 
-                    {isLoggedIn ? (
-                        <>
+                <nav className="navbar navbar-expand-sm navbar-light bg-light col-12">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
                             <li className="nav-item">
-                                <NavLink to="/add-recipe" activeClassName="active" className="nav-link">Add
-                                    Recipe</NavLink>
+                                <NavLink to="/" activeClassName="active" className="nav-link">Home</NavLink>
                             </li>
-                            <li className="nav-item">
-                                <NavLink to="/my-account" activeClassName='active' className='nav-link'>My account</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to="#" activeClassName='active' className='nav-link' onClick={() => logout(dispatch)}>Logout</NavLink>
-                            </li>
-                        </>
-                    ) : (
-                        <>
-                            <li className="nav-item">
-                                <NavLink to='/register' activeClassName='active'
-                                         className='nav-link'>Registration</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to='/login' activeClassName='active' className='nav-link'>Login</NavLink>
-                            </li>
-                        </>
-                    )}
-                </ul>
-            </nav>
-            <Route exact path="/">
-                {isLoggedIn ? <RecipeList user={state.user}/> : 'Please login to see your recipes'}
-            </Route>
-            <Route exact path="/recipe/:id">
-                {isLoggedIn ? <RecipeDetails/> : <h1>Please log in to view the recipe</h1>}
-            </Route>
 
-            <Route exact path="/add-recipe">
-                {isLoggedIn? <AddRecipe user={state.user}/> : <h1>Please log in to add a new recipe</h1>}
-            </Route>
-            <Route exact path="/edit-recipe/:id">
-                {isLoggedIn ? <EditRecipe user={state.user} /> : 'You are not logged in'}
-            </Route>
-            <Route exact path="/add-ingredient/:id" component={AddIngredient}/>
-            <Route exact path="/add-step/:id" component={AddStep}/>
-            <Route exact path="/register" component={UserRegistration}/>
-            <Route exact path="/edit-user-details">
-                {isLoggedIn ? <EditUserDetails user={state.user}/> : 'You are not logged in'}
-            </Route>
-            <Route exact path="/my-account" >
-                {isLoggedIn ? <UserAccount user={state.user}/> : 'You are not logged in'}
-            </Route>
-            <Route exact path="/login">
-                <UserLogin onLogin={(user) => dispatch({type: 'login', payload: user})}/>
-            </Route>
-        </Router>
+                            {isLoggedIn ? (
+                                <>
+                                    <li className="nav-item">
+                                        <NavLink to="/add-recipe" activeClassName="active" className="nav-link">Add
+                                            Recipe</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink to="/my-account" activeClassName='active' className='nav-link'>My
+                                            account</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink to="#" activeClassName='active' className='nav-link'
+                                                 onClick={() => logout(dispatch)}>Logout</NavLink>
+                                    </li>
+                                </>
+                            ) : (
+                                <>
+                                    <li className="nav-item">
+                                        <NavLink to='/register' activeClassName='active'
+                                                 className='nav-link'>Registration</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink to='/login' activeClassName='active'
+                                                 className='nav-link'>Login</NavLink>
+                                    </li>
+                                </>
+                            )}
+                        </ul>
+                    </div>
+                </nav>
+
+                <Route exact path="/">
+                    {isLoggedIn ? <RecipeList user={state.user}/> : 'Please login to see your recipes'}
+                </Route>
+                <Route exact path="/recipe/:id">
+                    {isLoggedIn ? <RecipeDetails/> : <h1>Please log in to view the recipe</h1>}
+                </Route>
+
+                <Route exact path="/add-recipe">
+                    {isLoggedIn ? <AddRecipe user={state.user}/> : <h1>Please log in to add a new recipe</h1>}
+                </Route>
+                <Route exact path="/edit-recipe/:id">
+                    {isLoggedIn ? <EditRecipe user={state.user}/> : 'You are not logged in'}
+                </Route>
+                <Route exact path="/add-ingredient/:id" component={AddIngredient}/>
+                <Route exact path="/add-step/:id" component={AddStep}/>
+                <Route exact path="/register" component={UserRegistration}/>
+                <Route exact path="/edit-user-details">
+                    {isLoggedIn ? <EditUserDetails user={state.user}/> : 'You are not logged in'}
+                </Route>
+                <Route exact path="/my-account">
+                    {isLoggedIn ? <UserAccount user={state.user}/> : 'You are not logged in'}
+                </Route>
+                <Route exact path="/login">
+                    <UserLogin onLogin={(user) => dispatch({type: 'login', payload: user})}/>
+                </Route>
+            </Router>
         </UserContext.Provider>
     );
 }
