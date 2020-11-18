40b9246559c50309a9656b4aa3a9cd23fdce77b0
Maksim Buturlakin <skvozik@gmail.com>
2020-11-18 00:12:43 -0800

    Add recipe delete feature

--- frontend/src/components/RecipeDetails.js
+++ frontend/src/components/RecipeDetails.js
@@ -7,6 +7,8 @@ import EditRecipe from "./EditRecipe";
 import IngredientsList from './RecipeDetails/IngredientsList';
 import StepList from './RecipeDetails/StepList'
 import UserContext from "../userContext";
+import ConfirmDelete from "./RecipeDetails/ConfirmDelete";
+
 const getRecipeURL = (id) => `http://127.0.0.1:8000/api/v1/recipes/${id}/`;
 
 function RecipeDetails(props) {
@@ -14,9 +16,11 @@ function RecipeDetails(props) {
     const [recipe, setRecipe] = useState();
     const [editRecipeMode, setEditRecipeMode] = useState(false);
     const [isEditable, setEditable] = useState(false)
+    const [show, setShow] = useState(false);
     const user = React.useContext(UserContext);
     const token = user.token;
     const user_id = user.id;
+    const deleteUrl = (id) => `http://127.0.0.1:8000/api/v1/recipes/${id}/`
 
     useEffect(() => {
         axios.get(getRecipeURL(id), {headers: {"Authorization": `Token ${token}`}})
@@ -26,37 +30,65 @@ function RecipeDetails(props) {
             })
     }, [editRecipeMode]);
 
+
+    function onAgreeToDelete() {
+        axios.delete(deleteUrl(recipe.id), {headers: {"Authorization": `Token ${token}`}}).then((response) => {
+            if (response.status === 204) {
+                console.log("Deleted succesfully");
+                props.history.push('/');
+            }
+        })
+    }
+
+    function onDisagree() {
+        setShow(false);
+    }
+
+    function onDelete() {
+        setShow(true);
+    }
+
     function displayRecipe() {
         const showEditForm = isEditable && editRecipeMode;
         return (
             <div className="col-lg-8">
-            <div className="card m-2 p-2">
-                {showEditForm ? (
-                        <EditRecipe
-                            user={user}
-                            id={recipe.id}
-                            onSave={() => {
-                                setEditRecipeMode(false)
-                            }}
-                        />)
-                    : (
-                        <>
-                            <h2>{recipe.name}
-                                {isEditable ? <i className="fas fa-pen h1icon" onClick={() => {
-                                    setEditRecipeMode(true)
-                                }}></i>  : null}
-                            </h2>
+                <div className="card m-2 p-2">
+                    {showEditForm ? (
+                            <EditRecipe
+                                user={user}
+                                id={recipe.id}
+                                onSave={() => {
+                                    setEditRecipeMode(false)
+                                }}
+                            />)
+                        : (
+                            <>
+                                <h2>{recipe.name}
+                                    {isEditable ?
+                                        <>
+                                            <i className="fas fa-pen h1icon" onClick={() => {
+                                                setEditRecipeMode(true)
+                                            }}></i>
+                                            <div style={{position: "relative"}} className="icon-div">
+                                                <i className="fas fa-trash-alt h1icon" onClick={onDelete}></i>
+                                                <ConfirmDelete onAgree={onAgreeToDelete} show={show}
+                                                               onDisagree={onDisagree}
+                                                               itemType={"recipe"}
+                                                />
+                                            </div>
+                                        </> : null}
+                                </h2>
 
-                            <img className="card-img" src={recipe.pic}/>
-                            <div className="card-text text-muted">{recipe.description}</div>
-                        </>
-                    )}
-                {recipe.ingredients_in_recipe ?
-                    <IngredientsList ingredients={recipe.ingredients_in_recipe} isEditable={isEditable}/> : null}
-                {recipe.steps_in_recipe ?
-                     <StepList steps={recipe.steps_in_recipe} isEditable={isEditable}/> : null}
+                                <img className="card-img" src={recipe.pic}/>
+                                <div className="card-text text-muted">{recipe.description}</div>
+                            </>
+                        )}
+                    {recipe.ingredients_in_recipe ?
+                        <IngredientsList ingredients={recipe.ingredients_in_recipe} isEditable={isEditable}/> : null}
+                    {recipe.steps_in_recipe ?
+                        <StepList steps={recipe.steps_in_recipe} isEditable={isEditable}/> : null}
 
-            </div>
+                </div>
             </div>
         )
     }
