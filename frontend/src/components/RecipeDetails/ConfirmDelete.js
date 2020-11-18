3b0b384ec4e7b97b66831aa487d56b00dc17b6cd
galubezzz <galubezzz@gmail.com>
2020-11-17 23:12:18 -0800

    add confrim delete popover for the ingredients

--- frontend/src/components/RecipeDetails/ConfirmDelete.js
+++ frontend/src/components/RecipeDetails/ConfirmDelete.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import $ from 'jquery';
+import React, {useState} from 'react';
 
-// $(function () {
-//   $('[data-toggle="example-popover"]').popover()
-// })
-git
 
-export default function ConfirmDelete(props){
-    const {onDelete} = props;
+export default function ConfirmDelete(props) {
+    const {onAgree, show, onDisagree} = props;
+    const classNames = show ? "popover fade show bs-popover-left" : "popover fade bs-popover-left";
+    const styles = {
+        top: "6px",
+        left: "-272px"
+    };
     return (
         <>
-            <div onClick={onDelete} data-toggle="example-popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Wonderful Delete component</div>
+            <div className={classNames} role="tooltip" x-placement="left" style={styles}>
+                <div className="arrow"></div>
+                <h3 className="popover-header">Alarm!</h3>
+                <div className="popover-body">
+                    Vivamus sagittis lacus vel augue laoreet rutrum faucibus.
+                </div>
+                <div>
+                    <button className="btn btn-outline-primary btn-sm m-2 popover-button" onClick={onAgree}>Ok</button>
+                    <button className="btn btn-outline-secondary btn-sm m-2 popover-button" onClick={onDisagree}>Cancel</button>
+                </div>
+            </div>
         </>
     )
 }
--- frontend/src/components/RecipeDetails/Ingredient.js
+++ frontend/src/components/RecipeDetails/Ingredient.js
@@ -4,31 +4,45 @@ import ConfirmDelete from "./ConfirmDelete";
 
 export default function Ingredient(props) {
     const {ingredient, onEdit, isEditable} = props;
-    const deleteUrl = (id) => `http://127.0.0.1:8000/api/v1/ingredients/${id}/`
+    const deleteUrl = (id) => `http://127.0.0.1:8000/api/v1/ingredients/${id}/`;
+    const [show, setShow] = useState(false);
 
-    function onDelete() {
-        let confirmation = window.confirm("Are you sure you want delete this ingredient?")
-        if (confirmation) {
-            axios.delete(deleteUrl(ingredient.id)).then((response) => {
-                if (response.status === 204) {
-                    console.log("Deleted succesfully");
-                    props.onDelete()
-                }
-            })
-        }
+    function onAgreeToDelete() {
+        console.log('agree to delete')
+        setShow(false);
+        axios.delete(deleteUrl(ingredient.id)).then((response) => {
+            if (response.status === 204) {
+                console.log("Deleted succesfully");
+                props.onDelete()
+            }
+        })
     }
 
-    // function drawDelete(){
-    //     setDeleteMode(true);
-    // }
+    function onDisagree() {
+        setShow(false);
+    }
+
+    function onDelete() {
+        setShow(true);
+        // $(function () {
+        //     $('[data-toggle="example-popover"]').popover()
+        // })
+        // let confirmation = window.confirm("Are you sure you want delete this ingredient?")
+        // if (confirmation) {
+        // }
+    }
+
     return (
         <>
             <li className="list-group-item">
                 <div>{ingredient.name} {ingredient.quantity}{ingredient.unit}
                     {isEditable ? <>
                         <i className="fas fa-pen icon" onClick={onEdit}></i>
-                        <i className="fas fa-trash-alt icon" onClick={onDelete}></i>
-                        {/*{deleteMode ? <ConfirmDelete onDelete={onDelete}/> : null}*/}
+                        <div style={{position: "relative"}} className="icon-div">
+                            <i className="fas fa-trash-alt icon" onClick={onDelete}></i>
+                            <ConfirmDelete onAgree={onAgreeToDelete} show={show} onDisagree={onDisagree}/>
+                        </div>
+
                     </> : null}
                 </div>
             </li>
