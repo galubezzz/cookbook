a5a33f5161952a626d66147447036bfb1133f0e2
Maksim Buturlakin <skvozik@gmail.com>
2020-11-17 17:39:28 -0800

    Add confirmDelete component for deletion prompts

---
+++ frontend/src/components/RecipeDetails/ConfirmDelete.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import $ from 'jquery';
+
+// $(function () {
+//   $('[data-toggle="example-popover"]').popover()
+// })
+
+
+export default function ConfirmDelete(props){
+    const {onDelete} = props;
+    return (
+        <>
+            <div onClick={onDelete} data-toggle="example-popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Wonderful Delete component</div>
+        </>
+    )
+}
