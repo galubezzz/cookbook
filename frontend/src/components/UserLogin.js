29c2917e47c2c24bf24640b26ed51ad92621c54b
galubezzz <galubezzz@gmail.com>
2020-11-27 00:31:24 -0800

    change styles for login page

--- frontend/src/components/UserLogin.js
+++ frontend/src/components/UserLogin.js
@@ -47,26 +47,47 @@ function UserLogin(props) {
     }
 
     return (
-        <form>
-            <div className="form-group row">
-                <label htmlFor="name" className="col-sm-1 col-form-label">Username:</label>
-                <div className="col-sm-8">
-                    <input name="name" id="name" className="form-control pr-2" onChange={changeUsername}/>
-                </div>
+        <div id="main">
+            <div className="container">
+                <div class="col-12 col-md-6">
+                        <div class="acc-box equal">
+                            <h2>Login</h2>
+                            <p><em>Welcome back. Log in using your e-mail and password.</em></p>
+                            <form>
+                              <div class="form-group">
+                                <label for="InputEmailAcc1">Username *</label>
+                                <input type="email" class="form-control" id="InputEmailAcc1" placeholder="Enter email" onChange={changeUsername}/>
+                              </div>
+                              <div class="form-group">
+                                <label for="exampleInputPassword1">Password *</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" onChange={changePassword}/>
+                              </div>
+                              <button type="submit" class="btn btn-primary" onClick={loginUser}>Login</button>
+                            </form>
+                        </div>
+                    </div>
+                {/*<form>*/}
+                {/*    <div className="form-group row">*/}
+                {/*        <label htmlFor="name" className="col-sm-1 col-form-label">Username:</label>*/}
+                {/*        <div className="col-sm-8">*/}
+                {/*            <input name="name" id="name" className="form-control pr-2" onChange={changeUsername}/>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*    <div className="form-group row">*/}
+                {/*        <label htmlFor="name" className="col-sm-1 col-form-label">Password:</label>*/}
+                {/*        <div className="col-sm-8">*/}
+                {/*            <input name="name" id="name" type="password" className="form-control pr-2"*/}
+                {/*                   onChange={changePassword}/>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*    <div className="form-group row">*/}
+                {/*        <div className="col-sm-9">*/}
+                {/*            <button className="btn btn-primary btn-block" onClick={loginUser}>Login</button>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</form>*/}
             </div>
-            <div className="form-group row">
-                <label htmlFor="name" className="col-sm-1 col-form-label">Password:</label>
-                <div className="col-sm-8">
-                    <input name="name" id="name" type="password" className="form-control pr-2"
-                           onChange={changePassword}/>
-                </div>
-            </div>
-            <div className="form-group row">
-                <div className="col-sm-9">
-                    <button className="btn btn-primary btn-block" onClick={loginUser}>Login</button>
-                </div>
-            </div>
-        </form>
+        </div>
     )
 }
 
