369a78d0a7a901d9a765c9f5322a87def4cf1347
Maksim Buturlakin <skvozik@gmail.com>
2020-11-26 20:11:11 -0800

    Add static_root parameter

--- cookbook/cookbook/settings.py
+++ cookbook/cookbook/settings.py
@@ -137,6 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
