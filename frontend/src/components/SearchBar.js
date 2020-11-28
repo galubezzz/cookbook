21fda83c3108492e83575329abad3ab21e11317e
galubezzz <galubezzz@gmail.com>
2020-11-27 22:01:40 -0800

    fix search animation and add header on user login page

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -7,33 +7,30 @@ function SearchBar(props) {
     const url = "http://127.0.0.1:8000/api/v1/recipes";
     const params = {};
     const searchUrl = (search) => `/search/${search}`;
+
     function search(event) {
         event.preventDefault();
         props.history.push(searchUrl(searchString))
     }
+
     function onSearchChange(event) {
         setSearchString(event.target.value)
     }
 
     return (
-        <div className="container searchbardiv" id="formsearch">
-                <form role="search" method="get" id="searchform" onSubmit={search}>
-                    <div className="input-group">
-                        <input type="text" id="searchbox" className="form-control" name="s" onChange={onSearchChange}/>
-                            <div className="input-group-btn">
-                                <button className="btn btn-search" id="searchsubmit" type="submit">
-                                    <strong>Search</strong>
-                                </button>
-                            </div>
+        <div className="container searchbardiv" id="formsearch" style={{display:"none"}}>
+            <form role="search" method="get" id="searchform" onSubmit={search}>
+                <div className="input-group">
+                    <input type="text" id="searchbox" className="form-control" name="s" onChange={onSearchChange}/>
+                    <div className="input-group-btn">
+                        <button className="btn btn-search" id="searchsubmit" type="submit">
+                            <strong>Search</strong>
+                        </button>
                     </div>
-                </form>
-            </div>
-        // <form className="form-inline" onSubmit={search}>
-        //     <input className="form-control mr-sm-2" type="search" placeholder="Search"
-        //            aria-label="Search" onChange={onSearchChange}/>
-        //     <button className="btn btn-outline-secondary my-2 my-sm-0" type="submit" >Search
-        //     </button>
-        // </form>
+                </div>
+            </form>
+        </div>
     )
 }
+
 export default withRouter(SearchBar)
