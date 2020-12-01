3207d4322d550e6d2c8c24e788ae69be3699482e
Maksim Buturlakin <skvozik@gmail.com>
2020-11-30 22:14:55 -0800

    Revert to http

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,5 +1,5 @@
 const env = process.env.NODE_ENV;
 
-export const baseUrl = env === 'development' ? 'http://127.0.0.1:8000' : 'https://18.222.87.201';
+export const baseUrl = env === 'development' ? 'http://127.0.0.1:8000' : 'http://18.222.87.201';
 
 console.log('=======', process.env.NODE_ENV);
