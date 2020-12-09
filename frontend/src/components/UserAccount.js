1cc32e997bad4136b062ca9a9d5cfa211138b8ed
Maksim Buturlakin <skvozik@gmail.com>
2020-12-09 00:25:03 -0800

    Make userAccount universal, delete recipes rendering from it

--- frontend/src/components/UserAccount.js
+++ frontend/src/components/UserAccount.js
@@ -3,32 +3,21 @@ import axios from 'axios';
 import {withRouter, Link} from "react-router-dom";
 import {baseUrl} from '../utils'
 import RecipeThumb from "./RecipeThumb";
+import UserRecipes from "./UserRecipes";
 
 const getUserURL = `${baseUrl}/api/v1/users/`;
 
 function UserAccount(props) {
     const [user, setUser] = useState();
     const [userParams, setUserParams] = useState();
-    const [params, setParams] = useState({});
-    const [favParams, setFavParams] = useState({...props.match.params, favorite: true});
+    const [favParams, setFavParams] = useState();
     const [heading, setHeading] = useState("");
-    let canEdit = props.user && props.user.username === props.match.params.username;
+    const [canEdit, setCanEdit] = useState(props.user && props.user.username === props.match.params.username);
 
-    const [recipes, setRecipes] = useState([]);
-    const [favRecipes, setFavRecipes] = useState([]);
-    const recipeUrl = `${baseUrl}/api/v1/recipes/`;
-
-
-
-    function RenderRecipes(recipes) {
-        return recipes.map((recipe) => {
-            return <RecipeThumb recipe={recipe} key={recipe.id}/>
-        })
-    }
 
     useEffect(() => {
         if (props.location.pathname === '/my-account/') {
-            canEdit = true;
+            setCanEdit(true);
             setHeading("My account");
         } else {
             setHeading(props.match.params.username + " account");
@@ -36,42 +25,25 @@ function UserAccount(props) {
     });
 
     useEffect(() => {
-        if (props.match.params) {
-            setUserParams(props.match.params);
-        } else {
+        if (props.location.pathname === '/my-account/') {
             setUserParams({username: props.user.username});
+        } else {
+            setUserParams(props.match.params);
         }
     }, []);
 
     useEffect(() => {
-
+    if (userParams){
         axios.get(getUserURL, {params: userParams}).then((response) => {
             setUser(response.data[0]);
-            // setParams({username: response.data[0].username});
-            setFavParams({username: response.data[0].username, favorite: true});
         });
-
+    }
 
     }, [userParams]);
 
-    useEffect(() => {
-        console.log("PARAMS!", userParams);
-        if (userParams) {
-            axios.get(recipeUrl, {params: userParams}).then((response) => {
-                setRecipes(response.data);
-            })
-        }
-    }, [userParams]);
 
-    useEffect(() => {
-        console.log("fav PARAMS!", favParams);
-        if (favParams) {
-            axios.get(recipeUrl, {params: favParams}).then((response) => {
-                setFavRecipes(response.data);
-                console.log(response.data);
-            })
-        }
-    }, [favParams]);
+
+
 
     return user ? (
         <>
@@ -108,43 +80,7 @@ function UserAccount(props) {
                                 </div>
 
 
-                                <div className="my-content">
-                                    <ul className="nav nav-tabs" id="myTab" role="tablist">
-                                        <li className="nav-item">
-                                            <a className="nav-link active" id="recipe-tab" data-toggle="tab"
-                                               href="#recipe"
-                                               role="tab"
-                                               aria-controls="recipe"
-                                               aria-selected="false">Recipes <strong>({recipes.length})</strong></a>
-                                        </li>
-                                        <li className="nav-item">
-                                            <a className="nav-link" id="bookmark-tab" data-toggle="tab" href="#bookmark"
-                                               role="tab" aria-controls="bookmark"
-                                               aria-selected="false">Bookmarked
-                                                Recipes <strong>({favRecipes.length})</strong></a>
-                                        </li>
-                                    </ul>
-                                    <div className="tab-content" id="myTabContent">
-                                        <div className="tab-pane fade show active" id="recipe" role="tabpanel"
-                                             aria-labelledby="recipe-tab">
-
-                                            <div className="row">
-                                                {recipes.length > 0 ? RenderRecipes(recipes) : "no recipes to show"}
-
-                                            </div>
-
-
-                                        </div>
-
-                                        <div className="tab-pane fade" id="bookmark" role="tabpanel"
-                                             aria-labelledby="bookmark-tab">
-                                            <div className="row">
-                                                {favRecipes.length > 0 ? RenderRecipes(favRecipes) : "no recipes to show"}
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                </div>
+                                {user ? <UserRecipes user={user}/> : null}
 
 
                             </div>
